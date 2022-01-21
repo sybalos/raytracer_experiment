152dd6e254d31ef9a6650ecde3026e63ce0706be
Sybalos <sybalos@gmail.com>
2022-01-21 11:24:51 -0500

    Casting ray basic

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
-
 from typeInfo import *
-
 from ray import Ray
 
-imageWidth = 256
-imageHeight = 256
-imageString = "P3\n{} {}\n255\n".format(imageWidth, imageHeight)
+def rayColor(ray):
+    unitDirection = glm.normalize(ray.direction)
+    t = 0.5 * (unitDirection.y + 1.0)
+    return (1.0-t) * Color(1.0,1.0,1.0) + t * Color(0.5,0.7,1.0)
+
+# Image
+
+aspectRatio = 16.0 / 9.0
+imageWidth = 400
+imageHeight = int(imageWidth / aspectRatio)
+
+# Camera
+
+viewportHeight = 2.0
+viewportWidth = aspectRatio * viewportHeight
+focalLength = 1.0
+
+origin = Point3(0,0,0)
+horizontal = Vec3(viewportWidth, 0, 0)
+vertical = Vec3(0,viewportWidth, 0)
+lowerLeftCorner = origin - horizontal/2 - vertical/2 - Vec3(0,0,focalLength)
+
+# Render
 
 def writeColor(outputString, color):
     ir = int(255.999 * color.r)
@@ -14,13 +32,18 @@ def writeColor(outputString, color):
     outputString += "{} {} {} \n".format(ir,ig,ib)
     return outputString
 
+imageString = "P3\n{} {}\n255\n".format(imageWidth, imageHeight)
 for j in reversed(range(imageHeight-1)):
     print("\rScanlines remaining: {} ", j)
     for i in range(imageWidth):
-        color = Color(float(i) / (imageWidth - 1), float(j) / (imageHeight - 1), 0.25 )
+        u = float(i) / (imageWidth - 1)
+        v = float(j) / (imageHeight - 1)
+        r = Ray(origin, lowerLeftCorner + u * horizontal + v*vertical - origin)
+        color = rayColor(r)
         imageString = writeColor(imageString, color)
 print("\nDone\n")
 
+rayColor(Ray(Color(0,0,0), Vec3(0,0,0)))
 
 file = open("result.ppm", "w")
 file.write(imageString)
